e10758b656a8507b4c71e3bf39f6ade364cd80c4
EUNGB <dldmsql114@gmail.com>
2021-03-18 18:10:36 +0900

    퍼블리싱 진행중 - About Us

---
+++ js/main.js
@@ -0,0 +1,12 @@
+new Swiper('.service .swiper-container', {
+  autoplay: true,
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 36,
+  navigation: {
+    prevEl: '.service .swiper-prev',
+    nextEl: '.service .swiper-next'
+  }
+})
+
+
